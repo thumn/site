cfcb465baac6d7a09a5f84c877c71270c78b1d26
Thu Nguyen <tmnguyen@berkeley.edu>
2020-12-19 01:25:17 -0800

    so apparently browserrouter doesnt work with ghpages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Photography from "./pages/Photography";
